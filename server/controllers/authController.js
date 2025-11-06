e7cdd8484cfe534d0527cbd87b4d142ab34ee543
Shan070106 <kit27.am51@gmail.com>
2025-11-06 22:45:54 +0530

    using Hotel model instead of User model, changes may happen in future

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../models/Hotel.js";
 
 const signToken = (userId) =>
   jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
--- server/controllers/hotelController.js
+++ server/controllers/hotelController.js
@@ -1,5 +1,12 @@
+// Importing Hotel model
+import Hotel from "../models/Hotel.js";
+import asyncHandler from "express-async-handler";
+
 const hotelController = {
-    
+    getHotel: asyncHandler(async (req, res) => {
+        const hotel = await Hotel.findById(req.params.id);
+        return res.json(hotel);
+    }),
 };
 
-export default hotelController ;
+export default hotelController;
