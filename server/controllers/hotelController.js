7f8f6bdf5c99cdd650c411e7574f2056ac654ef1
Shan070106 <kit27.am51@gmail.com>
2025-11-07 23:43:58 +0530

    in hotelRoutes,routes for  getting hotel-info and update info wrote for now

--- server/controllers/hotelController.js
+++ server/controllers/hotelController.js
@@ -2,11 +2,33 @@
 import Hotel from "../models/Hotel.js";
 import asyncHandler from "express-async-handler";
 
-const hotelController = {
-    getHotel: asyncHandler(async (req, res) => {
-        const hotel = await Hotel.findById(req.params.id);
-        return res.json(hotel);
-    }),
-};
+const getHotel = asyncHandler( async (req,res) => {
+    const hotelId = req.params.id;
+    try {
+        const hotel = await Hotel.findById(hotelId);
+        if(!hotel){
+            throw new Error("Hotel not found");
+        }
+        res.status(200).json(hotel);
+    } 
+    catch (error) {
+        res.status(404);
+        throw new Error("Hotel not found");
+    }
 
-export default hotelController;
+});
+
+const updateHotel = asyncHandler(async (req,res) => {
+    const hotelId = req.params.id;
+    const update = req.body;
+    try {
+        const hotel = await Hotel.findById(hotelId);
+        hotel.updateOne(update);
+        res.status(200).json({message: "Hotel updated successfully"});
+    } catch (error) {
+        res.status(400);
+        throw new Error("Failed to update hotel");
+    }
+});
+
+export {getHotel, updateHotel};
