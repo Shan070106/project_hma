f3db1f80d4883d839e87018eaccbec3b15db25e2
Shan070106 <kit27.am51@gmail.com>
2025-11-12 23:08:30 +0530

    hotel schema first version written

--- server/models/Hotel.js
+++ server/models/Hotel.js
@@ -1,5 +1,16 @@
 import mongoose from "mongoose";
 
-const hotelSchema = new mongoose.Schema();
+const hotelSchema = new mongoose.Schema(
+    {
+    hotelname: { type: String, required: true, trim: true },
+    address: { type: String, required: true, unique: true, trim: true },
+    rating: { type: Number, required: false,default:0, min: 0, max: 5},
+    description: { type: String, required: false, trim: true },
+    menu: [{
+            type: mongoose.Schema.Types.ObjectId, ref: "MenuItem"
+        },],
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Hotel", hotelSchema);
