b3e6c58a721e2f5704f0ba7e5fa4c549a578c1db
Shan070106 <kit27.am51@gmail.com>
2025-11-13 23:17:20 +0530

    first version of Menu model schema created

---
+++ server/models/Menu.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const menuSchema = new mongoose.Schema(
+    {
+        hotel: {type: mongoose.Schema.Types.ObjectId, ref: "Hotel", required: true},
+        name: {type: String, required: true, trim: true},
+        description: {type: String, required: false, trim: true},
+        amount: {type: Number, required: true, min: 0},
+        rating: {type: Number, required: false, default: 0, min: 0, max: 5},
+        image: {type: String, required: false},
+        recipe: {type: String, required: false, trim: true},
+    },
+    {
+        timestamps: true
+    }
+);
+
+export default mongoose.model("Menu",menuSchema);
