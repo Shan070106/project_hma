680b610b0e003d3065077ee38266bd92bcd1a977
Shan070106 <kit27.am51@gmail.com>
2025-11-27 23:22:26 +0530

    Menu item adding function included, not tested yet

---
+++ server/controllers/menuController.js
@@ -0,0 +1,49 @@
+import Menu from "../models/Menu.js";
+import Hotel from "../models/Hotel.js";
+import asyncHandler from "express-async-handler";
+
+const createMenu = asyncHandler(async (req,res) => {
+    const userId = req.userId;
+    
+    const hotel = await Hotel.findOne({user: userId});
+    if(!hotel){
+        res.status(404);
+        throw new Error("Hotel not found or create hotel first");
+    }
+
+    const { hotelId,name, description, amount, rating, image, recipe } = req.body || {};
+    if(!name || !amount || !hotelId){
+        res.status(400);
+        throw new Error("Please provide all required fields: name, amount, hotel");
+    }
+
+    const menu = await Menu.create({
+        hotel: hotelId,
+        name: name,
+        description: description,
+        amount: amount,
+        rating: rating,
+        image: image,
+        recipe: recipe,
+    });
+    res.status(201).json({ message:"Menu item created", menu });
+
+});
+
+const getMenuList = asyncHandler( async (req,res) => {
+
+});
+
+const getMenu = asyncHandler(async (req,res) => {
+
+});
+
+const updateMenu = asyncHandler(async (req, res) => {
+
+});
+
+const deleteMenu = asyncHandler(async (req,res) => {
+
+});
+
+export { createMenu, getMenuList, getMenu, updateMenu, deleteMenu };
