55c03127969541d0583cf8e40eaf4113bef2a4bf
Shan070106 <kit27.am51@gmail.com>
2025-12-04 22:51:14 +0530

    getMenu function first version

--- server/controllers/menuController.js
+++ server/controllers/menuController.js
@@ -35,7 +35,22 @@ const getMenuList = asyncHandler( async (req,res) => {
 });
 
 const getMenu = asyncHandler(async (req,res) => {
+    const userId = req.userId;
 
+    const hotel = await Hotel.findOne({user: userId});
+    if(!hotel){
+        res.status(404);
+        throw new Error("Hotel not found");
+    }
+
+    const menuId = req.params.menuId;
+    const menu = await Menu.findOne({id: menuId, hotel: hotel.id});
+    if(!menu){
+        res.status(404);
+        throw new Error("Menu item not found");
+    }
+
+    res.status(200).json({message: "Menu item that was asked",menu});
 });
 
 const updateMenu = asyncHandler(async (req, res) => {
