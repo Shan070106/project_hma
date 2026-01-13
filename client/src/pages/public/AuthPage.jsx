c8f397fa5e198ed668bed24bf746f802bf4adb98
Shan070106 <kit27.am51@gmail.com>
2026-01-13 20:38:28 +0530

    removed unused

--- client/src/pages/public/AuthPage.jsx
+++ client/src/pages/public/AuthPage.jsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom';
 import SignupPage from './SignupPage';
 import LoginPage from './LoginPage';
 import "../../assets/styles/AuthPage.css";
@@ -5,36 +6,11 @@ import "../../assets/styles/AuthPage.css";
 function AuthPage(){
     return (
         <div className='AuthPage'>
-            <nav><h1>Welcome to the Application !</h1></nav>
+            <nav><h1>Welcome to the Application !</h1>
+              <Link className = "home-btn" to="/">Home</Link>
+            </nav>
             <div className='Auth'>
                 <SignupPage />
-                {/* <div className='rules'>
-                    <h2>Validation Rules</h2>
-                      <h3>Email Address</h3>
-                      <ul>
-                        <li><p>Must be provided and cannot be empty</p></li>
-                        <li><p>Must follow a valid email format (e.g., user@domain.com)</p></li>
-                        <li><p>Must be unique (not already registered)</p></li>
-                        <li><p>Must be provided and cannot be empty</p></li>
-                      </ul>
-                      <h3>Password</h3>
-                      <ul>
-                        <li><p>Minimum length: 8 characters</p></li>
-                        <li><p>Must not be the same as the email or username</p></li>
-                        <li><p>One (A–Z),
-                            One (a–z),
-                            One (0–9),
-                            One special character (e.g., ! @ # $ % ^ & *)</p></li>
-                        <li><p>Must be provided and cannot be empty</p></li>
-                      </ul>
-                      <h3>Email Address</h3>
-                      <ul>
-                        <li><p>Must be provided and cannot be empty</p></li>
-                        <li><p>Must follow a valid email format (e.g., user@domain.com)</p></li>
-                        <li><p>Must be unique (not already registered)</p></li>
-                        <li><p>Must be provided and cannot be empty</p></li>
-                      </ul>
-                </div> */}
                 <LoginPage />
             </div>
         </div>
