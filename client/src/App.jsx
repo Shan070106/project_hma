ffc8f9e4c612527167e6a5a11dfb2b5cac7a2a0c
Shan070106 <kit27.am51@gmail.com>
2026-01-13 20:35:48 +0530

    removed unused

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
 import AdminLayout from './layouts/AdminLayout';
+import PublicLayout from './layouts/PublicLayout';
+
 import LandingPage from './pages/public/LandingPage';
 import AuthPage from './pages/public/AuthPage';
-import PublicLayout from './layouts/PublicLayout';
+
+import Dashboard from './pages/admin/Dashboard';
+import HotelPage from './pages/admin/HotelPage';
+import OrderPage from './pages/admin/OrderPage';
+import MenuPage from './pages/admin/MenuPage';
+import QrcodePage from './pages/admin/QrcodePage';
+
 
 function App(){
   return (
@@ -10,15 +19,22 @@ function App(){
         <Routes>
 
 
-        {/* Public */}
-        <Route element = {<PublicLayout />}>
-          <Route index element = {<LandingPage />} />
-          <Route path='/auth' element = {<AuthPage />}/>
-        </Route>
+          {/* Public */}
+          <Route element = {<PublicLayout />}>
+            <Route index element = {<LandingPage />} />
+            <Route path='/auth' element = {<AuthPage />}/>
+          </Route>
 
-        {/* Admin */}       
-        <Route element = {<AdminLayout />}>
-        </Route>
+          {/* Admin Protected*/}       
+          <Route element = {<AdminLayout />} >
+            <Route path='/admin'>
+              <Route index element = { <Dashboard/> } />
+              <Route path='/admin/hotel' element = { <HotelPage/> } />
+              <Route path='/admin/orders' element = { <OrderPage/> }/>
+              <Route path='/admin/menu' element = {<MenuPage/>} />
+              <Route path='/admin/qrcode' element = { <QrcodePage/>}/>
+            </Route>
+          </Route>
 
 
         </Routes>
