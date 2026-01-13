a1fd1b8b9b8b0c5a269ef16dc3edfd51abc14b0a
Shan070106 <kit27.am51@gmail.com>
2026-01-13 20:37:29 +0530

    removed unused

--- client/src/pages/public/LandingPage.jsx
+++ client/src/pages/public/LandingPage.jsx
@@ -1,9 +1,11 @@
-import { React } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PublicHeader from '../../components/Publics/publicHeader/PublicHeader';
+import '../../assets/styles/LandingPage.css'
 
 function LandingPage(){
     return (
-        <h1>HEllo WElcome to HMA</h1>
+        <div className='landing'>
+            <PublicHeader />
+        </div>
     );
 }
 
