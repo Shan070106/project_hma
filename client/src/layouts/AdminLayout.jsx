000cea35378abccfc27fe860b86c0e3bf456570b
Shan070106 <kit27.am51@gmail.com>
2026-01-14 22:35:16 +0530

    clean

--- client/src/layouts/AdminLayout.jsx
+++ client/src/layouts/AdminLayout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/admin/header/Header";
 import Sidebar from '../components/admin/sidebar/Sidebar';
 import '../assets/styles/AdminLayout.css';
-import Dashboard from "../pages/admin/Dashboard";
 
 function AdminLayout(){
     return (
