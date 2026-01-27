886a48fe9f49646f3af2c822d7a26d16625780b9
Shan070106 <kit27.am51@gmail.com>
2026-01-27 23:51:53 +0530

    component for displaying all menu with styles

---
+++ client/src/components/admin/menupage/DisplayMenu.jsx
@@ -0,0 +1,53 @@
+import './DisplayMenu.css';
+
+function MenuBox({key,menu,open}) {
+    return (
+        <tr>
+            <td className="td">
+                {menu?.image && <img src={menu.image} alt="Dish image" />}
+            </td>
+            <td className="td"> <p>{menu.name}</p></td>
+            <td className="td"><p>{menu.amount}</p></td>
+            <td className="td"><p>{menu.avail?"yes":"no"}</p></td>
+            <td>
+                <button className="menu-box" onClick={()=>(open(menu))}> open </button>
+            </td>
+        </tr>
+    );
+}
+
+
+function DisplayMenu({menus,open ,onBack}) {
+    const MenuList = menus.map(menuItem => {
+        return < MenuBox 
+            key={menuItem.id || menuItem.name}
+            menu={menuItem}
+            open={open}
+        />
+    });
+
+    return (
+        <>
+        <button type="button" onClick={onBack}>{"<-"}</button>
+            <table>
+        <caption>Displaying menu</caption>
+        
+        <thead>
+            <tr>
+                <th className="th">Image</th>
+                <th className="th">Name</th>
+                <th className="th">Price</th>
+                <th className="th">Availability</th>
+            </tr>
+        </thead>
+
+        <tbody>
+                  {MenuList}
+        </tbody>
+
+        </table>
+        </>
+    );
+}
+
+export default DisplayMenu;
