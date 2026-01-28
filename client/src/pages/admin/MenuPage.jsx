d581126239b9a6f6fcc550ea10c1045d48938f9c
Shan070106 <kit27.am51@gmail.com>
2026-01-28 23:07:28 +0530

    ux added in menupage

--- client/src/pages/admin/MenuPage.jsx
+++ client/src/pages/admin/MenuPage.jsx
@@ -1,5 +1,93 @@
-function MenuPage(){
-    return <h1>Menu page</h1>
+import { useState } from "react";
+
+import "../../assets/styles/MenuPage.css";
+import MenuSample from "../../sample data/MenuSample";
+import MenuForm from "../../components/admin/menupage/MenuForm";
+import DisplayMenu from "../../components/admin/menupage/DisplayMenu";
+
+function MenuPage() {
+    const [mode, setMode] = useState("list"); // mode : list | form 
+    const [opened, setOpened] = useState(null);
+    const [editable, setEdit] = useState(false);
+
+    // menuList fectched from server side later...
+    const menuList = MenuSample;
+    // const menuList = [];
+
+    const handleAdd = () => {
+        setOpened(null);
+        setMode("form");
+        setEdit(true);
+    }
+
+    const handleOpen = (menu) => {
+        setMode("form");
+        setEdit(false);
+        setOpened(menu);
+    }
+
+    const handleSave = (menuData) => {
+        console.log(menuData);
+        setEdit(false);
+        // setMode("list");
+        // setOpened(null);
+    }
+
+    const handleEdit = () => {
+        setEdit(true);
+    }
+
+    const handleCancel = () => {
+        setOpened(null);
+        setMode("list");
+        setEdit(false);
+    }
+
+    const onBack = () => {
+        setMode("list");
+        setOpened(null);
+        setEdit(false);
+    }
+
+    return (
+        <div className="menu-page">
+            <header>
+                <h2>Menu Page</h2>
+                <p>Add, delete, update, view all menu items here.</p>
+                <br />
+            </header>
+
+            <main>
+                {
+                    mode === "list" &&
+                    (
+                        <>
+                            <button onClick={handleAdd}>ADD</button>
+
+                            {menuList.length === 0 ? (
+                                <p>No menu added yet...!</p>
+                            ) : (
+                                <DisplayMenu menus={menuList} open={handleOpen} onBack={()=>setMode("list")}/>
+                            )}
+                        </>
+                    )
+
+                }
+
+                {
+                    mode === "form" &&
+                    < MenuForm
+                        menu={opened}
+                        edit={editable}
+                        onSave={handleSave}
+                        onCancel={handleCancel}
+                        onEdit={handleEdit}
+                        onBack={() => setMode("list")}
+                    />
+                }
+            </main>
+        </div>
+    );
 }
 
 export default MenuPage;
