85c641380353792b44f785b7963e38bf4ace4eac
Shan070106 <kit27.am51@gmail.com>
2026-01-30 11:35:17 +0530

    image field updated

--- server/models/Menu.js
+++ server/models/Menu.js
@@ -7,7 +7,10 @@ const menuSchema = new mongoose.Schema(
         description: {type: String, required: false, trim: true},
         amount: {type: Number, required: true, min: 0},
         rating: {type: Number, required: false, default: 0, min: 0, max: 5},
-        image: {type: String, required: false},
+        image: {
+                url: { type: String, required: true},
+                publicId: { type: String, required: false}
+            },
         recipe: {type: String, required: false, trim: true},
         avail: {type: Boolean, required: true,default: true}
     },
