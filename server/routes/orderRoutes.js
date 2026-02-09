74c5e77a88c19c0fbf6c3c66cfb9cb2955480d44
Shan070106 <kit27.am51@gmail.com>
2026-02-09 21:15:45 +0530

    made a small change

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -6,6 +6,6 @@ const router = Router();
 router.post('/:hotelId',createOrder);
 router.get('/:hotelId/session/:sessionId/table/:tableId',getCustomerOrder);
 router.patch('/:hotelId/session/:sessionId/table/:tableId/cancel',cancelOrder);
-router.get('/hotel/all',requireAuth,customerOrders);
+router.get('/hotel/all',customerOrders);
 
 export default router;
