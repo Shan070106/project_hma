a3d0c48eff3428b508aba3fbc0462069565b166b
Shan070106 <kit27.am51@gmail.com>
2026-02-14 23:28:22 +0530

    useState added for sign up info variables

--- client/src/pages/public/SignupPage.jsx
+++ client/src/pages/public/SignupPage.jsx
@@ -1,30 +1,45 @@
+import { useState } from "react";
 import "../../assets/styles/SignupPage.css";
 
 function SignupPage(){
 
+  const [user, setUser] = useState({
+    email : "",
+    username: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    const {key,value} = e.target;
+    setUser({
+      ...preUser,
+      [key] : value
+    });
+  }
+ 
   return (
     <div className="Signup">
       <h2>Sign Up</h2>
-      <form className="signup-form">
+      <form className="signup-form" onSubmit={handleSubmit}>
 
         <div className="signup-row">
           <label htmlFor="mail"><b>Email</b></label> 
-          <input type="email" placeholder="Email"/>
+          <input type="email" placeholder="Email" onChange={handleChange}/>
         </div>
         
          <div className="signup-row">
           <label htmlFor="name"><b>Username</b></label> 
-          <input type="text" placeholder="Your Username"/>
+          <input type="text" placeholder="Your Username" onChange={handleChange}/>
         </div>
 
          <div className="signup-row">
           <label htmlFor="pswd"><b>Password</b></label> 
-          <input type="password" placeholder="Key to application"/>
+          <input type="password" placeholder="Key to application" onChange={handleChange}/>
         </div>
 
          <div className="signup-row">
           <label htmlFor="confirm-pswd"><b>Confirm Password</b></label> 
-          <input type="password" placeholder="Finalize Key"/>
+          <input type="password" placeholder="Finalize Key" onChange={handleChange}/>
         </div>
 
         <button className="submit-btn" type="submit">Submit</button>
