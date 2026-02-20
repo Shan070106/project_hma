ba0fb9c9acd142ceb3f440c7d7c47dfc2b587395
Shan070106 <kit27.am51@gmail.com>
2026-02-20 11:15:47 +0530

    email box removed since we dont handle login using email, and functions for handling changes,success and error added

--- client/src/pages/public/LoginPage.jsx
+++ client/src/pages/public/LoginPage.jsx
@@ -1,24 +1,69 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+
 import "../../assets/styles/LoginPage.css";
 
 function LoginPage(){
+  const [user, setUser] = useState({
+    username:"",
+    password:""
+  });
+
+  const navigateTo = useNavigate();
+
+  const {username,password} = user;
+
+  const handleChange = (e) => {
+    const {name,value} = e.target;
+    setUser({
+      ...user,
+      [name] : value
+    });
+  }
+
+  const handleSuccess = (successMessage) => {
+    toast.success(successMessage,{
+      position: "top-center"
+    });
+  }
+
+  const handleError = (errorMessage) => {
+    toast.error(errorMessage, {
+      position: "top-center"
+    });
+  }
+
   return (
     <div className="Login">
       <h2>Login</h2>
       <form className="login-form">
-
-        <div className="login-row">
-          <label htmlFor="mail"><b>Email</b></label> 
-          <input type="email" placeholder="Email"/>
-        </div>
         
          <div className="login-row">
           <label htmlFor="name"><b>Username</b></label> 
-          <input type="text" placeholder="Your Username"/>
+          <input 
+            type="text" 
+            id="username"
+            name="username"
+            value={username}
+            onChange={handleChange}
+            placeholder="Your Username"
+            required
+          />
         </div>
 
          <div className="login-row">
           <label htmlFor="pswd"><b>Password</b></label> 
-          <input type="password" placeholder="Key to application"/>
+          <input 
+            type="password" 
+            id="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            placeholder="Key to application"
+            required
+          />
         </div>
 
         <button className="submit-btn" type="submit">Submit</button>
