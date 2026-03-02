4106fd634a3acfbce16095d08b5a6ecd360b5845
Shan070106 <kit27.am51@gmail.com>
2026-03-02 14:37:05 +0530

    Hotel schema updated, added few more properties

--- server/models/Hotel.js
+++ server/models/Hotel.js
@@ -2,10 +2,21 @@ import mongoose from "mongoose";
 
 const hotelSchema = new mongoose.Schema(
     {
-    hotelname: { type: String, required: true, trim: true },
-    address: { type: String, required: true, unique: true, trim: true },
-    rating: { type: Number, required: false,default:0, min: 0, max: 5},
+    name: { type: String, required: true, trim: true },
     description: { type: String, required: false, trim: true },
+    images: [ {
+                url: { type: String, required: false } ,
+                publicId: { type: String, required: false}
+              }
+            ],
+    address: { type: String, required: true, unique: true, trim: true },
+    city: {type: String, required: true, trim: true},
+    state: {type: String,required: true, trim: true},
+    country: {type: String, required: true, trim: true},
+    pincode: {type: String, required: true, max: 6},
+    phone: {type: String, required: true, trim: true},
+    email: {type: String, required:true, unique: true, lowercase: true, trim: true},
+    rating: { type: Number, required: false,default:0, min: 0, max: 5},
     menu: [
             { type: mongoose.Schema.Types.ObjectId, ref: "MenuItem"},
           ],
